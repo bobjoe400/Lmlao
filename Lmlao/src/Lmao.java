b626a0ce29c4424bf4bc69bea7041b70c4660a93
Cooper <Cooper@10.151.19.194>
2016-05-22 22:06:38 -0700

    labo

--- Lmlao/src/Lmao.java
+++ Lmlao/src/Lmao.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class Lmao {
 	public static void main(String[] args){
-	
+		Scanner in = new Scanner(System.in);
+		System.out.println('a'-1+1);
+		int pos = in.nextLine().charAt(0) - 'a';
+		System.out.println(pos);
+		in.close();
 	}
 }
